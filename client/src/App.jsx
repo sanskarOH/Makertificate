72a7b38ef21e1b10ec048e1a9bdfb26f36a9ead3
sanskarOH <sanskaryeeha@gmail.com>
2025-09-27 04:57:20 +0530

    chore:basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-6xl font-extrabold '>Makertificate</h1>
+        <h1 className='text-3xl '>Work in progress</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
